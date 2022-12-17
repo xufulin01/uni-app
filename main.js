0403f1cb35d90968374d4e72c9988e5be7029531
xufulin01 <xufulin1995@163.com>
2022-12-17 15:24:55 +0800

    安装数据请求包，轮播图制

--- main.js
+++ main.js
@@ -2,7 +2,34 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+/**
+ * 请求包的加载
+ */
+import {$http}from"@escook/request-miniprogram"
+/**
+ * @param {Object} option挂载到uni实例
+ */
+uni.$http=$http;
 
+/**
+ * 设置请求根路径
+ */
+$http.baseUrl= 'https://api-hmugo-web.itheima.net'
+/**
+ * @param {Object} option请求拦截器
+ */
+$http.beforeRequest=function(option){
+	uni.showLoading({
+		title:"数据请求中...",
+		
+	})
+}
+/**
+ * @param {Object} option请求之后
+ */
+$http.afterRequest=function(option){
+	uni.hideLoading()
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		home
+	    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+	    	<swiper-item v-for="(item,i) in swiperList" :key="i" class="swi">
+	    		<view class="swiper-item">
+					<img :src="item.url"></img>
+				</view>
+	    	</swiper-item>
+	    	
+	    </swiper>
 	</view>
 </template>
 
@@ -8,12 +15,43 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList:[
+					{
+						id:1,
+						url:"https://img0.baidu.com/it/u=1618970823,1350528377&fm=253&fmt=auto&app=138&f=JPEG?w=650&h=203"
+					},{
+						id:2,
+						url:"https://img0.baidu.com/it/u=4221556751,3134031808&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=220"
+					},{
+						id:3,
+						url:"https://img1.baidu.com/it/u=118763193,2397896178&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500"
+					}
+				]
 			};
+		},
+		onLoad(){
+			this.getSwiperList()
+		},
+		methods:{
+			async getSwiperList(){
+				const res=await uni.$http.get("/api/public/v1/home/swoperdata")
+				console.log(res)
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.swi{
+	width: 100%;
+	height: 300px;
+	.swiper-item{
+		width: 100%;
+		height: 100%;
+		img{
+			width: 100%;
+			height: 100%;
+		}
+	}
+}
 </style>
